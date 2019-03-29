42933cf3c0de8a366340e35a42a564ea98ad6edb
Quang <quocquang1997@gmail.com>
2019-03-29 09:52:52 +0700

    Update CategoryController.php

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -24,7 +24,7 @@ class CategoryController extends Controller
                 'name'=>'required',
             ],
             [
-                'name.required'=>'Bạn chưa nhập tên hãng.'
+                'name.required'=>'Ban chua nhap ten thuong hieu.'
             ]);
         $category = new Category();
         $category->name = $request->name;
