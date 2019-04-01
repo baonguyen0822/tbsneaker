481362148fd0534018b8a195dbc1f641eeb3f565
quanng1997 <48916406+quanng1997@users.noreply.github.com>
2019-04-01 12:31:39 +0700

    Update confirm.blade.php

--- resources/views/page/cart/confirm.blade.php
+++ resources/views/page/cart/confirm.blade.php
@@ -5,7 +5,7 @@
 		<div class="container">
 			<div class="breadcrumb-banner d-flex flex-wrap align-items-center justify-content-end">
 				<div class="col-first">
-					<h1>Xác nhận đơn hàng</h1>
+					<h1>Xác nhận đơn Hàng</h1>
 					<nav class="d-flex align-items-center">
 						<a href="trangchu">Trang chủ<span class="lnr lnr-arrow-right"></span></a>
 						<a style="color:white;">Xác nhận đơn hàng</a>
